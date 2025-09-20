46076ab926d56a0b1290da02d46612e2b9fcd08f
Dineth Bandara <131736551+Dinethzz@users.noreply.github.com>
2025-09-20 19:28:21 +0530

    Add PricingSection component and integrate it into the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import PricingSection from "../components/PricingSection";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans">
+      {/* Header Section */}
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -98,6 +101,10 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      </div>
+      
+      {/* Pricing Section */}
+      <PricingSection />
     </div>
   );
 }
